a04ca05eab7cf31642e115ce3d99924c27924b90
Dani05 <dani229@abv.bg>
2019-03-14 14:06:39 +0200

    Fixed DB Name

--- Server/database/database.js
+++ Server/database/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const User = require('../models/User');
 mongoose.Promise = global.Promise;
 module.exports = () => {
-    mongoose.connect('mongodb://localhost:27017/TestDb', {
+    mongoose.connect('mongodb://localhost:27017/TimelessClassicsDB', {
         useNewUrlParser: true
     });       
     const db = mongoose.connection;
